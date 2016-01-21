dc7b9a0d7dce22ccf0a5cf1764a4875b5fd5b512
sathish.s <sathish.257216@gmail.com>
2016-01-21 19:42:05 +0530

    email testcases updated

--- src/test/java/com/example/service/EmailServiceTest.java
+++ src/test/java/com/example/service/EmailServiceTest.java
@@ -1,6 +1,7 @@
 package com.example.service;
 
 import com.example.AbstractTest;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,10 +22,40 @@ public class EmailServiceTest extends AbstractTest {
 
     @Test
     public void testSendingMail() throws MessagingException {
-        String email = "xxx@gmail.com";
+        String email = "puri.twinkle@gmail.com";
         String subject = "Forgot Password";
         String mailBody = "http://localhost:8080/#/login";
-
-        emailServiceImpl.send(email, subject, mailBody);
+        boolean valid = emailServiceImpl.send(email, subject, mailBody);
+        Assert.assertEquals("failure - expected 200 success", valid);
+        Assert.assertTrue("failure - expected true",valid);
     }
+
+    @Test
+    public void testSendingMailHandlingException() throws MessagingException {
+        String email = "xxxxxxxx";
+        String subject = "Forgot Password";
+        String mailBody = "http://localhost:8080/#/login";
+        boolean valid = emailServiceImpl.send(email, subject, mailBody);
+        Assert.assertNotEquals("failure - expected error",valid);
+        Assert.assertFalse("failure - expected true",valid);
+    }
+
+    @Test
+    public void testSendingAsyncMail() {
+        String email = "puri.twinkle@gmail.com";
+        String subject = "Forgot Password";
+        String mailBody = "http://localhost:8080/#/login";
+        emailServiceImpl.sendAsync(email, subject, mailBody);
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void testSendingAsyncMailHandlingException() {
+        String email = "sfdssf";
+        String subject = "Forgot Password";
+        String mailBody = "http://localhost:8080/#/login";
+        emailServiceImpl.sendAsync(email, subject, mailBody);
+        Assert.assertFalse(false);
+    }
+
 }
