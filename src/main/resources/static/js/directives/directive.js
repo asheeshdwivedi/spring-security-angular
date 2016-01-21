d09dc34c1085300c95f8d9600913ef60460889c3
asheeshdwivedi <asheesh.dwivedi@hotmail.com>
2016-01-20 23:57:03 -0500

    Refactor code

--- src/main/resources/static/js/directives/directive.js
+++ src/main/resources/static/js/directives/directive.js
@@ -6,3 +6,27 @@ springSecurityAngular.directive("emailForm" ,function(){
     }
 
 });
+
+springSecurityAngular.directive("confirmPassword" ,function(){
+    return {
+        restrict :'E',
+        templateUrl : 'partials/directives/confirmPassword.html',
+        replace : true,
+    }
+
+});
+
+springSecurityAngular.directive("confirmPasswordCheck" , function(){
+
+    return {
+        require: 'ngModel',
+        link: function (scope, elem, attrs, ctrl) {
+            var firstPassword = '#' + attrs.confirmPasswordCheck;
+            elem.add(firstPassword).on('keyup', function () {
+                scope.$apply(function () {
+                    ctrl.$setValidity('confirmPassMatch', elem.val() === $(firstPassword).val());
+                });
+            });
+         }
+        }
+});
